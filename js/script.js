b6493feb2a8b3c3eebdaf278c92bff7ce3111b1d
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-04-19 10:01:59 +0300

    add business logic function in js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//BUSINESS LOGIC
+
+function Order(type, size, crust, topping) {
+  this.type = type;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+}
+
+Order.prototype.fullOrder = function () {
+  return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
+};
+
+function Total(price, quantity, delivery) {
+  this.price = price;
+  this.quantity = quantity;
+  this.delivery = delivery;
+}
+
+Total.prototype.finalTotal = function () {
+  return this.price * this.quantity + this.delivery;
+};
+
+
+var sizePrice = [1400, 900, 500]
+var deliverPrices = [0, 300];
